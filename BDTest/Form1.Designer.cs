9305ad947a6ae45e0aa845fdbbc38980bf47441b
Pedro <salazar9_5@hotmail.com>
2025-04-07 09:32:59 -0700

    registro de persona

--- BDTest/Form1.Designer.cs
+++ BDTest/Form1.Designer.cs
@@ -28,13 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tbNombre = new System.Windows.Forms.TextBox();
+            this.tbAP = new System.Windows.Forms.TextBox();
+            this.tbAM = new System.Windows.Forms.TextBox();
+            this.tbCiudad = new System.Windows.Forms.TextBox();
+            this.tbEdad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btRegistrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbNombre
+            // 
+            this.tbNombre.Location = new System.Drawing.Point(29, 26);
+            this.tbNombre.Name = "tbNombre";
+            this.tbNombre.Size = new System.Drawing.Size(100, 20);
+            this.tbNombre.TabIndex = 0;
+            // 
+            // tbAP
+            // 
+            this.tbAP.Location = new System.Drawing.Point(183, 26);
+            this.tbAP.Name = "tbAP";
+            this.tbAP.Size = new System.Drawing.Size(100, 20);
+            this.tbAP.TabIndex = 1;
+            // 
+            // tbAM
+            // 
+            this.tbAM.Location = new System.Drawing.Point(361, 26);
+            this.tbAM.Name = "tbAM";
+            this.tbAM.Size = new System.Drawing.Size(100, 20);
+            this.tbAM.TabIndex = 2;
+            // 
+            // tbCiudad
+            // 
+            this.tbCiudad.Location = new System.Drawing.Point(29, 92);
+            this.tbCiudad.Name = "tbCiudad";
+            this.tbCiudad.Size = new System.Drawing.Size(100, 20);
+            this.tbCiudad.TabIndex = 3;
+            // 
+            // tbEdad
+            // 
+            this.tbEdad.Location = new System.Drawing.Point(183, 92);
+            this.tbEdad.Name = "tbEdad";
+            this.tbEdad.Size = new System.Drawing.Size(100, 20);
+            this.tbEdad.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(87, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Apellido Paterno:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(358, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Apellido Materno:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Ciudad:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(180, 76);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Edad:";
+            // 
+            // btRegistrar
+            // 
+            this.btRegistrar.Location = new System.Drawing.Point(208, 140);
+            this.btRegistrar.Name = "btRegistrar";
+            this.btRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.btRegistrar.TabIndex = 10;
+            this.btRegistrar.Text = "Registrar";
+            this.btRegistrar.UseVisualStyleBackColor = true;
+            this.btRegistrar.Click += new System.EventHandler(this.btRegistrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(492, 212);
+            this.Controls.Add(this.btRegistrar);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbEdad);
+            this.Controls.Add(this.tbCiudad);
+            this.Controls.Add(this.tbAM);
+            this.Controls.Add(this.tbAP);
+            this.Controls.Add(this.tbNombre);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox tbNombre;
+        private System.Windows.Forms.TextBox tbAP;
+        private System.Windows.Forms.TextBox tbAM;
+        private System.Windows.Forms.TextBox tbCiudad;
+        private System.Windows.Forms.TextBox tbEdad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btRegistrar;
     }
 }
 
--- BDTest/Form1.cs
+++ BDTest/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace BDTest
 {
@@ -15,6 +17,34 @@ namespace BDTest
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btRegistrar_Click(object sender, EventArgs e)
+        {
+            string nombre = tbNombre.Text;
+            string ap = tbAP.Text;
+            string am=tbAM.Text;
+            string ciudad = tbCiudad.Text;
+            string edad=tbEdad.Text;
+
+            MySqlConnection conn = new MySqlConnection("server=127.0.0.1;uid=root;pwd=123456;database=pruebas");
+            MySqlCommand comm = new MySqlCommand("insert into personas(nombre,apellidoP,apellidoM,ciudad,edad)" +
+                $"values ('{nombre}','{ap}','{am}','{ciudad}','{edad}');", conn);
+            comm.CommandType = CommandType.Text;
+            try
+            {
+                conn.Open();
+                comm.ExecuteNonQuery();
+                MessageBox.Show("Guardado correctamente");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            finally
+            {
+                conn.Close();
+            }
         }
     }
 }
